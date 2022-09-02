12eabc0d60d1339ceed73ab9f040946310887ae3
Simone Gentili <sensorario@gmail.com>
2022-09-02 14:30:22 +0200

    feat: add empty constructor

--- tongue.go
+++ tongue.go
@@ -1,3 +1,4 @@
 package tongue
 
-
+func LoadDictionary() {
+}
--- tongue_test.go
+++ tongue_test.go
@@ -1,15 +1,9 @@
 package tongue
 
-
 import (
 	"testing"
-	"fmt"
 )
 
-type trans struct {
-}
-
-func TestReturnTranslation(t *testing.T) {
-    tt := []trans{}
-    fmt.Println(tt)
+func TestDictionaryCanBeLoadedByFunc(t *testing.T) {
+    LoadDictionary()
 }

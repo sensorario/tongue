7db973d3aacd2eaa8c7d28d0fdd7f49b5026a3ab
Simone Gentili <sensorario@gmail.com>
2022-09-02 18:13:14 +0200

    fix: provide map and create all translations

--- tongue_test.go
+++ tongue_test.go
@@ -2,8 +2,36 @@ package tongue
 
 import (
 	"testing"
+	"strings"
+	"reflect"
+	"fmt"
 )
 
 func TestDictionaryCanBeLoadedByFunc(t *testing.T) {
-    LoadDictionary()
+    d := LoadDictionary()
+    d.Add("it", "foo", "bar")
+    sentence := d.Get("it", "foo") 
+    if sentence != "bar" {
+        t.Fatal(strings.Join([]string{
+            "Ops ",
+            "'", sentence, "'", 
+            " should be equal to bar",
+        }, ""))
+    }
+}
+
+func TestGenerateMap(t *testing.T) {
+    d := LoadDictionary()
+    d.Add("it", "foo", "bar")
+    d.Add("en", "foo", "barr")
+
+    mappa := make(map[string]map[string]string)
+    mappa["it"] = make(map[string]string)
+    mappa["it"]["foo"] = "bar"
+    mappa["en"] = make(map[string]string)
+    mappa["en"]["foo"] = "barr"
+
+    if !reflect.DeepEqual(mappa, d.Map()) {
+        t.Fatal("Oops! Generated map is not correct")
+    }
 }
